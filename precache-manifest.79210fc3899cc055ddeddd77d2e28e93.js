3b67aa07211e364d161c113d5fbc42eb31794073
tranle1411 <chantranle_2026@depauw.edu>
2025-03-27 12:42:17 -0400

    Updates

---
+++ precache-manifest.79210fc3899cc055ddeddd77d2e28e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8c075a43e2147599c4f90b7a1c72f8",
+    "url": "/wageinsight/index.html"
+  },
+  {
+    "revision": "a4dc34e58d44abca8ce6",
+    "url": "/wageinsight/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/wageinsight/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/wageinsight/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "a4dc34e58d44abca8ce6",
+    "url": "/wageinsight/static/js/main.fffdfd66.chunk.js"
+  },
+  {
+    "revision": "4e5e1bb559ecea0f36b2",
+    "url": "/wageinsight/static/js/runtime~main.a3a9c850.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wageinsight/static/media/logo.06e73328.svg"
+  }
+]);
