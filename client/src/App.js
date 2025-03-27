f528cc82c94d0fbcd4d56bcf979b8fa0815554b6
tranle1411 <chantranle_2026@depauw.edu>
2025-03-27 17:10:45 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-const API_URL = "https://wageinsight.onrender.com";
+const API_URL = "https://wageinsight.onrender.com/predict";
 
 function App() {
   return (
